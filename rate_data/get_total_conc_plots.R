d504b5be3a1d93e02b5c8c633579dd99968e90ea
swo <swo@mit.edu>
2013-12-12 13:29:23 -0500

    added total concentration plots

---
+++ rate_data/get_total_conc_plots.R
@@ -0,0 +1,24 @@
+library(RColorBrewer)
+col <- rev(colorRampPalette(brewer.pal(9, 'RdBu'))(50))
+
+files <- c('conc_1.csv','conc_2.csv','conc_3.csv','conc_4.csv','conc_5.csv','conc_6.csv','conc_7.csv','conc_8.csv', 'conc_9.csv', 'conc_10.csv')
+names <- c('O', 'C', 'N+', 'N-', 'S+', 'S-', 'Fe+', 'Fe-', 'CO2', 'CH4')
+imax <- length(files)
+
+par(mfrow=c(2, 5), mar=c(4,2,1,1), new=F)
+for (i in 1: imax) {
+    file <- files[i]
+    name <- names[i]
+    
+    cat(c(file, rate, "\n"))
+    raw <- read.csv(file, header=F)
+    mat <- as.matrix(raw)
+    totals <- colSums(mat)
+    
+    bot <- min(totals) #signif(min(totals), 2)
+    top <- max(totals) #signif(max(totals), 2)
+    plot(totals, xlab=name, xaxt='n', yaxp=c(bot, top, 1), ylim=c(bot, top), type='l')
+    
+    # need some fancy footwork with matrix so that time=depth=0 appears at top left
+    #image(t(apply(mat, 2, rev)), col=col, bty='n', xaxt='n', yaxt='n', xlab=name, cex.lab=1.0)
+}
