4d3e8f9aca7cf6040424f0c8f1f8d5230c9292ec
swo <swo@mit.edu>
2013-12-17 12:02:04 -0500

    calibration has nonnegative range
    Modified calibrate.m so that parameters are all nonnegative.
    Added a README.

--- calibration/write_calibrate.py
+++ calibration/write_calibrate.py
@@ -15,6 +15,7 @@ conf.read('../lake.cfg')
 param_names, param_values = zip(*conf.items('Simulation parameters'))
 param_names_list = ", ".join(param_names)
 param_values_list = ", ".join(param_values)
+n_params = len(param_names)
 
 # write the base calibration script
 with open('calibrate.m.template', 'r') as f:
@@ -23,6 +24,7 @@ with open('calibrate.m.template', 'r') as f:
 script_content = template
 script_content = re.sub('__VARIABLE_NAMES_HERE__', param_names_list, script_content)
 script_content = re.sub('__VARIABLE_VALUES_HERE__', param_values_list, script_content)
+script_content = re.sub('__N_VARIABLES__', str(n_params), script_content)
 
 with open('calibrate.m', 'w') as f:
     f.write(script_content)
