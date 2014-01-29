8a7938f99bf4c588fc2628e4d967ced67d5f8631
swo <swo@mit.edu>
2014-01-29 14:36:58 -0500

    removed jsd_plots
    they are now folded into "analysis"

--- jsd_plots/pipeline.sh
+++
@@ -1,7 +0,0 @@
-cp rates.csv rates.csv.bak
-
-# throw away iron oxidation on oxygen
-drop_column.pl 0 rates.csv.bak > rates.csv
-
-# convert the rates rows into a matrix of JSDs
-python -c "import survey.analyze as sa; sa.csv2jsd()"
