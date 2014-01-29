357c2b11ac2df0e3ad9e2c17c1024dd147dbc1c7
swo <swo@mit.edu>
2014-01-29 14:35:29 -0500

    added divergences and diversity plots
    both run using the pipeline
    better to have history soft-linked to the interactive or whatever
    axes, etc. aren't nice yet

---
+++ analysis/get_divergences.m
@@ -0,0 +1,34 @@
+% load up the data
+load('history.mat');
+
+[n_t, n_d, n_r] = size(rates_history);
+divergences = zeros(n_d, n_d);
+
+for i = 1: n_d
+    for j = 1: n_d
+        % get the rows
+        r1 = rates_history(end, i, :);
+        r2 = rates_history(end, j, :);
+
+        % normalize rows
+        r1 = r1 / sum(r1);
+        r2 = r2 / sum(r2);
+
+        % get arthmetic mean
+        avg = 0.5 * (r1 + r2);
+
+        % removes zeros
+        r1 = r1(r1 > 0.0);
+        r2 = r2(r2 > 0.0);
+        avg = avg(avg > 0.0);
+
+        % get the divergence, equally weighted
+        divergences(i, j) = -sum(avg .* log(avg)) - 0.5 * (-sum(r1 .* log(r1)) + -sum(r2 .* log(r2)));
+    end
+end
+
+h = pcolor(divergences);
+axis square
+set(h, 'edgecolor', 'none');
+
+print('plots/divergences.pdf', '-dpdf')
