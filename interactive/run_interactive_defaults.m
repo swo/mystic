f93eecf3a1aef5ceb10fbe8c3f53f5dea5481be3
swo <swo@mit.edu>
2014-03-21 11:59:04 -0400

    in the process of tweaking variables

--- interactive/run_interactive_defaults.m
+++ interactive/run_interactive_defaults.m
@@ -1,11 +1,11 @@
 [time_slices, concs_history, rates_history] = lake( ...
 	0.0,	... oxygen_bubble_rate
-	800.0,	... nitrogen_source
+	20.0,	... nitrogen_source
 	0.1,	... nitrogen_ratio
 	75000.0,	... fixed_carbon_level
-	2.2e4,	... fixed_oxygen_level
-	10000.0,	... fixed_oxygen_diffusion
-	6000.0,	... fixed_bottom_methane
+	1.7e4,	... fixed_oxygen_level
+	0.0,	... fixed_oxygen_diffusion
+	730.0,	... fixed_bottom_methane
 	2.0,	... t_max
 	0.1,	... fe_precipitation
 	1.0,	... diff_const_comp
