28c8b5da0d48908da7c7b17b6bc43db465430579
swo <swo@mit.edu>
2014-03-24 16:29:51 -0400

    tweaks

--- interactive/run_interactive_defaults.m
+++ interactive/run_interactive_defaults.m
@@ -3,7 +3,7 @@
 	0.0,	... nitrogen_source
 	0.1,	... nitrogen_ratio
 	4.5e4,	... carbon_source
-	0.5e4,	... oxygen_source
+	0.35e4,	... oxygen_source
 	1500.0,	... methane_source
 	1.0,	... t_max
 	0.3,	... fe_precipitation
@@ -14,7 +14,7 @@
 	0.16,	... ma_op_o_s_rate_const
 	0.01,	... ma_op_fe_n_rate_const
 	1000.0,	... ma_op_ch4_o_rate_const
-	0.01,	... ma_op_ch4_s_rate_const
+	0.001,	... ma_op_ch4_s_rate_const
 	1,	... primary_ox_rate_const
 	20.0,	... c_lim_o
 	5.0,	... c_lim_n
@@ -23,9 +23,9 @@
 	200.0,	... concs0_c
 	50.0,	... concs0_o
 	100.0,	... concs0_ntot
-	1.0,	... pm_ratio_n
+	10.0,	... pm_ratio_n
 	60.0,	... concs0_fetot
-	1.0,	... pm_ratio_fe
-	200.0,	... concs0_stot
-	1.0	... pm_ratio_s
+	10.0,	... pm_ratio_fe
+	100.0,	... concs0_stot
+	10.0	... pm_ratio_s
 );
