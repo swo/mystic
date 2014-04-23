1e2454547e9ff5d962c3bbeb83397725642e578c
swo <swo@mit.edu>
2014-04-23 12:38:16 -0400

    sens: improved sigfigs

--- sensitivity_analysis/bin/get_rates.m
+++ sensitivity_analysis/bin/get_rates.m
@@ -1,11 +1,11 @@
-function [] = get_rates(nitrogen_ratio, carbon_ratio, fixed_oxygen_level, fixed_oxygen_diffusion, fixed_bottom_methane, t_max, fe_precipitation, diff_const_comp, ma_op_o_fe_rate_const, ma_op_o_n_rate_const, ma_op_o_s_rate_const, ma_op_fe_n_rate_const, ma_op_ch4_o_rate_const, ma_op_ch4_s_rate_const, primary_ox_rate_const, c_lim_o, c_lim_n, c_lim_fe, c_lim_s, concs0_c, concs0_o, concs0_ntot, pm_ratio_n, concs0_fetot, pm_ratio_fe, concs0_stot, pm_ratio_s, rates_fn, concs_fn)
+function [] = get_rates(oxygen_bubble_rate, nitrogen_source, nitrogen_ratio, carbon_source, oxygen_source, methane_source, t_max, fe_precipitation, carbon_precipitation, diffusion_constant, ma_op_o_fe_rate_const, ma_op_o_n_rate_const, ma_op_o_s_rate_const, ma_op_fe_n_rate_const, ma_op_ch4_o_rate_const, ma_op_ch4_s_rate_const, primary_ox_rate_const, c_lim_o, c_lim_n, c_lim_fe, c_lim_s, concs0_c, concs0_o, concs0_ntot, pm_ratio_n, concs0_fetot, pm_ratio_fe, concs0_stot, pm_ratio_s, rates_out_fn, concs_out_fn)
 
-[time_slices, concs_history, rates_history] = lake(nitrogen_ratio, carbon_ratio, fixed_oxygen_level, fixed_oxygen_diffusion, fixed_bottom_methane, t_max, fe_precipitation, diff_const_comp, ma_op_o_fe_rate_const, ma_op_o_n_rate_const, ma_op_o_s_rate_const, ma_op_fe_n_rate_const, ma_op_ch4_o_rate_const, ma_op_ch4_s_rate_const, primary_ox_rate_const, c_lim_o, c_lim_n, c_lim_fe, c_lim_s, concs0_c, concs0_o, concs0_ntot, pm_ratio_n, concs0_fetot, pm_ratio_fe, concs0_stot, pm_ratio_s);
+[time_slices, concs_history, rates_history] = lake(oxygen_bubble_rate, nitrogen_source, nitrogen_ratio, carbon_source, oxygen_source, methane_source, t_max, fe_precipitation, carbon_precipitation, diffusion_constant, ma_op_o_fe_rate_const, ma_op_o_n_rate_const, ma_op_o_s_rate_const, ma_op_fe_n_rate_const, ma_op_ch4_o_rate_const, ma_op_ch4_s_rate_const, primary_ox_rate_const, c_lim_o, c_lim_n, c_lim_fe, c_lim_s, concs0_c, concs0_o, concs0_ntot, pm_ratio_n, concs0_fetot, pm_ratio_fe, concs0_stot, pm_ratio_s);
 
 final_rates = squeeze(rates_history(end, :, :));
-dlmwrite(rates_out_fn, final_rates);
+dlmwrite(rates_out_fn, final_rates, precision=15);
 
 final_concs = squeeze(concs_history(end, :, :));
-dlmwrite(concs_out_fn, final_concs);
+dlmwrite(concs_out_fn, final_concs, precision=15);
 
 end
